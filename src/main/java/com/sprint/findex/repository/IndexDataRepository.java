3c90d8567158c54f3a2aac89e054bd47b9c9034f
ParkSungGuk <cdjsdj1902@gmail.com>
2026-03-17 13:57:39 +0900

    fix: merge과정에서 살아나ë²버린 코드 삭제

--- src/main/java/com/sprint/findex/repository/IndexDataRepository.java
+++ src/main/java/com/sprint/findex/repository/IndexDataRepository.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.UUID;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -26,10 +25,6 @@ public interface IndexDataRepository extends JpaRepository<IndexData, UUID>,
             @Param("startDate") LocalDate startDate,
             @Param("endDate") LocalDate endDate,
             Sort sort);
-
-    @Modifying
-    @Query("delete from IndexData d where d.indexInfo.id = :indexInfoId")
-    void deleteAllByIndexInfoId(UUID indexInfoId);
 
     long countByIndexInfoId(UUID indexInfoId);
 }
